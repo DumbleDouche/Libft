7970c1397d1a121b5251f32428e7c5bcb6976c42
DumbleDouche <www.ronan.c@gmail.com>
2017-01-12 18:24:15 +0100

    Forgot the right include

--- src/ft_putstr_fd.c
+++ src/ft_putstr_fd.c
@@ -6,11 +6,12 @@
 /*   By: rchoquer <rchoquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 19:16:55 by rchoquer          #+#    #+#             */
-/*   Updated: 2017/01/12 18:17:19 by rchoquer         ###   ########.fr       */
+/*   Updated: 2017/01/12 18:23:38 by rchoquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+#include <unistd.h>
 
 void	ft_putstr_fd(char const *s, int fd)
 {
