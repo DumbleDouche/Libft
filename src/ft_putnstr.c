0fc79e6302e7cfb1f5d0cf8339acc863d3e698ed
DumbleDouche <www.ronan.c@gmail.com>
2017-07-10 20:48:28 +0200

    modified ft_putnstr.c

--- src/ft_putnstr.c
+++ src/ft_putnstr.c
@@ -6,7 +6,7 @@
 /*   By: rchoquer <rchoquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 01:44:25 by rchoquer          #+#    #+#             */
-/*   Updated: 2016/11/19 01:49:54 by rchoquer         ###   ########.fr       */
+/*   Updated: 2017/07/10 20:33:47 by rchoquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 void	ft_putnstr(const char *string, size_t size)
 {
-	size_t	i;
-
-	i = 0;
-	while (size--)
-	{
-		write(1, &string[i], 1);
-		i++;
-	}
+	if (string)
+		write(1, &string, size);
 }
