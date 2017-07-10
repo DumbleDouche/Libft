e660e4dbe0af6115f5fd6e584b79a9ee6efcee29
DumbleDouche <www.ronan.c@gmail.com>
2017-07-10 21:03:13 +0200

    correcting &string to string

--- src/ft_putnstr.c
+++ src/ft_putnstr.c
@@ -6,7 +6,7 @@
 /*   By: rchoquer <rchoquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 01:44:25 by rchoquer          #+#    #+#             */
-/*   Updated: 2017/07/10 20:33:47 by rchoquer         ###   ########.fr       */
+/*   Updated: 2017/07/10 21:02:08 by rchoquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 void	ft_putnstr(const char *string, size_t size)
 {
 	if (string)
-		write(1, &string, size);
+		write(1, string, size);
 }
